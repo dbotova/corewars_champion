d72b4680a6bbf0c09ea927a95d1a7907393fabaa
Darya Botova <darya.botova@hotmail.com>
2017-04-11 21:42:57 -0700

    Add files via upload
    zork is down

--- moonlight.s
+++ moonlight.s
@@ -3,15 +3,12 @@
 .name "moonlight"
 .comment "You wiil be punished in the name of the Moon"
 
-l0:			sti r1, %:live, %1
-			sti r1, %:moondust, %1
+l0:			sti r1, %:moondust, %1
+			sti r1, %:live, %1
 			sti r1, %:l1, %1
-			sti r1, %:l0, %1
 			sti r1, %:reborn, %1
-			live %0
-			lfork %:l1
-			live %0
-			zjmp %:l0
+			live %928638
+			lfork %:l0
 
 reborn:		st r1, 00
 			st r1, 1024
@@ -20,51 +17,68 @@ reborn:		st r1, 00
 			st r1, -1
 			st r1, -5000
 			st r1, -24
-			live %0
-			zjmp %:l0
+			live %100
 			zjmp %:reborn
-			fork %:l0
+			fork %:reborn
 
-l1:			live %0
+l1:			live %4902343
 			st r1, 00
 			st r1, 1024
 			st r1, -1024
 			st r1, -6000
 			ld %-1, r10
 			ld %-789, r4
-			lfork %:l0
-			live %0
+			lfork %:l1
+			live %-50
 			ld %-100, r10
 			ld %-50, r4
 
-live:		live %0
-			sti r1, %:l0, %1
-			fork %:l0
-			sti r1, %:l0, %1
-			zjmp %:l0
-			zjmp %:reborn
-			fork %:moondust
+live:		live %2343
+			sti r1, %:l0, %1100
+			fork %:l1
 
-moondust:	st r5, -100
-			st r5, -90
-			st r5, -110
-			st r5, -80
-			st r5, -120
-			st r5, -70
-			st r5, -130
-			st r5, -60
-			st r5, -140
-			st r5, -50
-			st r5, -150
-			st r5, -40
-			st r5, -160
-			st r5, -30
-			st r5, -170
-			st r5, -20
-			st r5, -180
-			st r5, -10
-			st r5, -190
+moondust:	st r3, -100
+			st r3, -90
+			st r3, -110
+			st r3, -80
+			st r3, -120
+			st r3, -70
+			st r3, -130
+			st r3, -60
+			st r3, -140
+			st r3, -50
+			st r3, -150
+			st r3, -40
+			st r3, -160
+			st r3, -30
+			st r3, -170
+			st r3, -20
+			st r3, -180
+			st r3, -10
+			st r3, -190
+			st r3, 0
+			st r3, 200
+			add r1, r10, r3
+			st r3, 100
+			st r3, 90
+			st r3, 110
+			st r3, 80
+			st r3, 120
+			st r3, 70
+			st r3, 130
+			st r3, 60
+			st r3, 140
+			st r3, 50
+			st r3, 150
+			st r3, 40
+			st r3, 160
+			st r3, 30
+			st r3, 170
+			st r3, 20
+			st r3, 180
+			st r3, 10
+			st r3, 190
+			st r3, 0
+			st r3, 200
+			add r1, r10, r3 
 			zjmp %:moondust
-			lfork %:l0
-			fork %:l0
-			fork %:moondust
